f9df02bd5c2caf0b8db752b84fbd828fa389b81f
tjkessler <travis.j.kessler@gmail.com>
2021-07-12 16:23:14 -0400

    Docstrings, tokenizing now works

--- vae/chemvae.py
+++ vae/chemvae.py
@@ -7,6 +7,19 @@ import torch.nn as nn
 class ChemVAE(nn.Module):
 
     def __init__(self, input_dim: int, latent_dim: int = 4, hidden_dim: dict = 8):
+        """
+        ChemVAE: variational auto-encoder leveraging ELBO loss, designed to
+        encode/decode SMILES strings into/out of a probabilistic latent space;
+        child of torch.nn.Module
+
+        Args:
+            input_dim (int): number of input features; also acts as the number
+                of outputs when decoding
+            latent_dim (int, optional): number of features/probabilitic
+                distributions in the encoder's latent space (default: 4)
+            hidden_dim (int, optional): number of neurons between input/latent
+                and latent/output (default: 8)
+        """
 
         super(ChemVAE, self).__init__()
         self._input_dim = input_dim
@@ -20,6 +33,18 @@ class ChemVAE(nn.Module):
         self.fc4 = nn.Linear(self._hidden_dim, self._input_dim)
 
     def encode(self, x: 'torch.tensor') -> Tuple['torch.tensor', 'torch.tensor']:
+        """
+        Encodes input data to latent distributions
+
+        Args:
+            x (torch.tensor): input/unencoded data, shape [n_samples,
+                n_features]
+
+        Returns:
+            Tuple[torch.tensor, torch.tensor]: (latent distr. means, latent
+                distr. logvar i.e. log(sigma^2)); shape of means and logvars is
+                [n_samples, latent_dim]
+        """
 
         z = T.sigmoid(self.fc1(x))
         mean = T.sigmoid(self.fc2a(z))
@@ -27,6 +52,16 @@ class ChemVAE(nn.Module):
         return (mean, logvar)
 
     def decode(self, z: 'torch.tensor') -> 'torch.tensor':
+        """
+        Given samples (assumed from mean/logvar distribution), decode to
+        original dimensionality
+
+        Args:
+            z (torch.tensor): samples, shape [n_samples, latent_dim]
+
+        Returns:
+            torch.tensor: decoded data, shape [n_samples, n_features]
+        """
 
         z = T.sigmoid(self.fc3(z))
         z = T.sigmoid(self.fc4(z))
@@ -34,12 +69,40 @@ class ChemVAE(nn.Module):
 
     @staticmethod
     def generate(mean: 'torch.tensor', logvar: 'torch.tensor') -> 'torch.tensor':
+        """
+        Given latent means and logvars, generate data in latent dimension;
+        generation follows:
+
+        $ mu + (N(0,1) * e^{0.5 * logvar})$
+
+        Args:
+            mean (torch.tensor): latent distribution mean values, shape
+                [n_samples, latent_dim]
+            logvar (torch.tensor): latent distribution logvar values, shape
+                [n_samples, latent_dim]
+
+        Returns:
+            torch.tensor: generated data, shape [n_samples, latent_dim]
+        """
 
         stdev = T.exp(0.5 * logvar)
         noise = T.randn_like(stdev)
         return mean + (noise * stdev)
 
     def forward(self, x: 'torch.tensor') -> Tuple['torch.tensor', 'torch.tensor', 'torch.tensor']:
+        """
+        torch.nn.Module forward operation; given input data, return
+        reconstructed data, mean/logvar in latent space
+
+        Args:
+            x (torch.tensor): input data, shape [n_samples, n_features]
+
+        Returns:
+            Tuple[torch.tensor, torch.tensor, torch.tensor]: (reconstructed
+                data, latent mean, latent logvar); reconstructed data of shape
+                [n_samples, n_features], latent mean/logvar of shape
+                [n_samples, latent_dim]
+        """
 
         mean, logvar = self.encode(x)
         inpt = self.generate(mean, logvar)
@@ -48,6 +111,19 @@ class ChemVAE(nn.Module):
 
 
 def elbo_loss(recon_x: 'torch.tensor', x: 'torch.tensor', mean: 'torch.tensor', logvar: 'torch.tensor', beta: float = 1.0):
+    """
+    Evidence lower bound (ELBO) loss function, from
+    https://arxiv.org/abs/1312.6114
+
+    Args:
+        recon_x (torch.tensor): reconstructed/decoded data, shape [n_samples,
+            n_features]
+        x (torch.tensor): input data, shape [n_samples, n_features]
+        mean (torch.tensor): latent mean, shape [n_samples, latent_dim]
+        logvar (torch.tensor): latent logvar, shape [n_samples, latent_dim]
+        beta (float, optional): scalar for Kullback-Leibler divergence portion
+            of loss (default: 1.0)
+    """
     # https://arxiv.org/abs/1312.6114
 
     bce = T.nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
--- vae/tokenizer.py
+++ vae/tokenizer.py
@@ -2,23 +2,26 @@ from typing import List
 import collections
 import re
 
-import torch
-import torch as T
-
 
 class SMILESTokenizer(object):
 
-    def __init__(self):
+    def __init__(self, X: List[str], n_merges: int = 16):
+        """
+        Fits tokenizer to SMILES strings, taking into account subwords
+        (frequently observed groups of characters, often functional groups)
+        using byte pair encoding (BPE)
 
-        self._vocab = {}
-        self._n_bits_per_word = 8
-
-    def fit(self, X: List[str], n_merges: int = 4):
-        '''
         Adapted from https://arxiv.org/pdf/1508.07909.pdf
-        '''
+
+        Args:
+            X (List[str]): SMILES strings to fit tokenizer
+            n_merges (int): number of merges (iterations of BPE)
+        """
 
         _words = [' '.join(smi) for smi in X]
+
+        self._best = []
+        self._p = []
 
         for i in range(n_merges):
 
@@ -27,42 +30,49 @@ class SMILESTokenizer(object):
                 symbols = word.split()
                 for i in range(len(symbols) - 1):
                     pairs[symbols[i], symbols[i + 1]] += 1
-
             best = max(pairs, key=pairs.get)
 
-            _new_words = []
             bigram = re.escape(' '.join(best))
             p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
+
+            _new_words = []
             for word in _words:
                 _new_words.append(p.sub(''.join(best), word))
-
             _words = _new_words
 
-        self._vocab = {}
-        for word in _words:
-            chars = word.split(' ')
-            for c in chars:
-                if c not in self._vocab.keys():
-                    self._vocab[c] = 1
-                else:
-                    self._vocab[c] += 1
+            self._best.append(best)
+            self._p.append(p)
 
-    def fit_transform(self, X: List[str], n_merges: int = 4) -> 'torch.tensor':
+    def tokenize(self, X: List[str]) -> List[List[str]]:
+        """
+        Tokenize SMILES strings using pre-trained BPE vocabulary
 
-        self.fit(X, n_merges)
-        return self.transform(X)
+        Args:
+            X (List[str]): SMILES strings to tokenize
 
-    def transform(self, X: List[str]) -> 'torch.tensor':
+        Returns:
+            List[List[str]]: tokenized SMILES strings, each list element
+                represents a SMILES string; each element is a list of "words"
+                (subgroups defined by BPE) that make up each SMILES string
+        """
 
-        trf_vals = []
-        for smi in X:
-            trf_vals.append(self._tokenize(smi))
-        return trf_vals  # TODO: to torch.tensor
+        _words = [' '.join(smi) for smi in X]
 
-    def inverse_transform(self, X: 'torch.tensor') -> List[str]:
+        for i in range(len(self._p)):
+
+            _new_words = []
+            for word in _words:
+                _new_words.append(
+                    self._p[i].sub(''.join(self._best[i]), word)
+                )
+            _words = _new_words
+
+        _words = [w.split(' ') for w in _words]
+        return _words
+
+
+class SMILESEmbedder(object):
+
+    def __init__(self):
 
         return
-
-    def _tokenize(self, smiles: str):
-
-        return  # TODO: this next
